4bdd1e0d2fa7912a3271a2c0e8d70180bb692677
N4sonxxx <gerrymuchlisient@gmail.com>
2025-08-20 01:17:15 +0800

    fix darkmode lightmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // This line is essential for dark mode
+  darkMode: 'class',
+  
+  // This tells Tailwind where to look for its class names
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
